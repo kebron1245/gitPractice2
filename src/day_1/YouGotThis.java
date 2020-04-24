dd00750ed8a45dfc7204cb93607919b3daa4546f
kebron1245 <64176610+kebron1245@users.noreply.github.com>
2020-04-24 16:29:41 -0400

    Update YouGotThis.java

--- src/day_1/YouGotThis.java
+++ src/day_1/YouGotThis.java
@@ -4,5 +4,8 @@ public class YouGotThis {
     public static void main(String[] args) {
 
         System.out.println("Just Do It");
+        
+        
+        // This line is added from Github mate
     }
 }
